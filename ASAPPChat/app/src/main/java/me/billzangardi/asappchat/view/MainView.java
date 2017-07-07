2bd9d8fdb0e6b646bc384cdcc04603f78919ea9d
Bill Zangardi <bill.zangardi@gmail.com>
2017-07-07 16:30:55 -0400

    Initial project commit

---
+++ ASAPPChat/app/src/main/java/me/billzangardi/asappchat/view/MainView.java
@@ -0,0 +1,8 @@
+package me.billzangardi.asappchat.view;
+
+/**
+ * Created by zangardiw on 7/7/17.
+ */
+
+public interface MainView {
+}
---
+++ ASAPPChat/app/src/main/java/me/billzangardi/asappchat/view/impl/MainActivity.java
@@ -0,0 +1,15 @@
+package me.billzangardi.asappchat.view.impl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.billzangardi.asappchat.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
